68ded36a975f5cebdd58457ba3edbe4317b712aa
Riffat Hasan <riffat.hasan8@gmail.com>
2025-05-22 18:32:17 +0600

    blog-fullw

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -5,7 +5,7 @@
             <div class="flex gap-x-10">
         <div v-for="blog in blogs" :key="blog" class="relative">
             <div >
-                <img :src="blog.image">
+                <img :src="blog.image" class="w-full">
             </div>
             <div class="blog_text w-full absolute top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] text-center px-5 py-10 bg-[rgba(0,0,0,0.3)]">
         <h3 class="font-sig text-[16px] font-semibold text-white">{{blog.title}}</h3>
